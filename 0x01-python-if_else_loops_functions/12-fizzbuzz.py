042920138b47851317a7e8e897721be018babece
abrish-seng <abrish814@gmail.com>
2024-01-02 10:34:19 -0800

    printing fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,18 @@
+def fizzbuzz():
+    """Print the numbers from 1 to 100 separated by a space.
+
+    For multiples of three, print Fizz instead of the number.
+    For multiples of five, print Buzz instead of the number.
+    For multiples of three and five, print FizzBuzz instead of the number.
+    """
+    accomulator = f''
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            accomulator += "FizzBuzz "
+        elif num % 5 == 0:
+            accomulator += "Buzz "
+        elif num % 3 == 0:
+            accomulator += f'Fizz '
+        else:
+            accomulator += f'{num} '
+    print(accomulator, end="")
