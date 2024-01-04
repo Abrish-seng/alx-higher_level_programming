47b49f6f4b5efe000b58967ebef9445c19e8237e
abrish-seng <abrish814@gmail.com>
2024-01-04 06:14:18 -0800

    adding a and b

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-import add_0 as wc
+if __name__ == "__main__":
+    """Print the sum of 1 and 2"""
+    from add_0 import add
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a,b,wc.add(a,b)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
