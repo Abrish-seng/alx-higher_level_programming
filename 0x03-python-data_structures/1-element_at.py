46c21965bf18e7f6899437a5dcda04f4adf0038b
abrish-seng <abrish814@gmail.com>
2024-01-06 04:56:06 -0800

    printing elements from the list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 def element_at(my_list, idx):
-    """List an element from the list."""
+    """ Retrieves an element from a list like in C """
     if idx < 0 or idx > (len(my_list) - 1):
-        return (None)
-        
+        return None
+
     return (my_list[idx])
