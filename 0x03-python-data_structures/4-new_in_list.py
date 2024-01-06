ce289776cd9417942ba67d52615169aeff339b00
abrish-seng <abrish814@gmail.com>
2024-01-05 22:54:36 -0800

    new list added

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
+    if idx < 0 or idx > (len(my_list) - 1):
         return my_list
     new_file = [i for i in my_list]
     new_file[idx] = element
