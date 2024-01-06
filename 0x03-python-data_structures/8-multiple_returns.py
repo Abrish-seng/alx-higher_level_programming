b0debe06b0e2a44928fdd4422686a2e9181e7144
abrish-seng <abrish814@gmail.com>
2024-01-05 23:23:34 -0800

    printing the max vlaue

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@
 def multiple_returns(sentence):
     
     if sentence == "":
-        return (0,None)
+        return (0, None)
     return (len(sentence), sentence[0])
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    max = 0
-    for i in range(0, len(my_list) - 1):
+    max = my_list[0]
+    for i in range(len(my_list) - 1):
         if my_list[i] > max:
             max = my_list[i]
     return max
