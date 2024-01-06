4299545596319f51ff076341f605788283f6912a
abrish-seng <abrish814@gmail.com>
2024-01-06 05:05:49 -0800

    deleting element from the list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,10 @@
 def max_integer(my_list=[]):
     """ Function that finds the biggest integer of a list """
     if len(my_list) == 0:
-        return None
-    max = my_list[0]
-    for i in range(len(my_list) - 1):
-        if my_list[i] > max:
-            max = my_list[i]
-    return max
+        return (None)
+
+    big = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > big:
+            big = my_list[i]
+    return (big)
