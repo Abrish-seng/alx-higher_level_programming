aee2735430e6c4a9d85f72a9d1d9ee666a018190
abrish-seng <abrish814@gmail.com>
2024-01-10 08:21:47 -0800

    fixing the key printing error

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    """Print the key in the dictionary."""
-    return (len(a_dictionary.key()))
+    """returns the number of keys in a dictionary."""
+    return len(a_dictionary.keys())
