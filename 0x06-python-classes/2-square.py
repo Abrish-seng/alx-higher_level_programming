09574c953e591d125f6454ee4e7fa644016cf455
abrish-seng <abrish814@gmail.com>
2024-01-23 07:47:29 -0800

    error with raise fixed

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,5 +8,5 @@ class Square:
         if (type(size) is not int):
             raise TypeError("size must be an integer")
         elif (size < 0):
-            ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self._size = size
