4afbcb7ebf691c6ac19a4dd1fe2179cb0c8f1c82
abrish-seng <abrish814@gmail.com>
2024-01-23 05:45:18 -0800

    fixed

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Empty class created."""
+"""Square class to represent a square"""
+
 
 class Square():
-    """The square class is created."""
-
+    """Empty class"""
     pass
