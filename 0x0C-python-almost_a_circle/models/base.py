58d2803cbaf639bdb60fe4280b3ebddc2655995a
abrish-seng <abrish814@gmail.com>
2024-02-12 09:37:57 -0800

    pycodestyle fixed

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,7 +11,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize instances."""   
+        """Initialize instances."""
         if id is not None:
             self.id = id
         else:
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,7 +95,7 @@ class Rectangle(Base):
 
     def update(self, *args, **kwargs):
         """Update method."""
-        if args != None and len(args) != 0:
+        if args is not None and len(args) != 0:
             list_atr = ['id', 'width', 'height', 'x', 'y']
             for i in range(len(args)):
                 setattr(self, list_atr[i], args[i])
