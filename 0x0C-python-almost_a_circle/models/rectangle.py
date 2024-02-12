2ed04fc53a23acbc48d1d01cebfc6e3eaf7b317b
abrish-seng <abrish814@gmail.com>
2024-02-12 09:24:31 -0800

    square files and rectangle files are crated

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+"""Module that contains class Rectangle, inheritance of class Base."""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Class Rectangle."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize instances."""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Width getter."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Width setter."""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Height getter."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Height setter."""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """X getter."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """X setter."""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Y getter."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Y setter."""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Return the area of the rectangle object."""
+        return self.width * self.height
+
+    def display(self):
+        """Display a rectangle."""
+        rectangle = self.y * "\n"
+        for i in range(self.height):
+            rectangle += (" " * self.x)
+            rectangle += ("#" * self.width) + "\n"
+
+        print(rectangle, end='')
+
+    def __str__(self):
+        """Str special method."""
+        str_rectangle = "[Rectangle] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_wh = "{}/{}".format(self.width, self.height)
+
+        return str_rectangle + str_id + str_xy + str_wh
+
+    def update(self, *args, **kwargs):
+        """Update method."""
+        if args != None and len(args) != 0:
+            list_atr = ['id', 'width', 'height', 'x', 'y']
+            for i in range(len(args)):
+                setattr(self, list_atr[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return a dictionary with properties."""
+        list_atr = ['id', 'width', 'height', 'x', 'y']
+        dict_res = {}
+
+        for key in list_atr:
+            dict_res[key] = getattr(self, key)
+
+        return dict_res
